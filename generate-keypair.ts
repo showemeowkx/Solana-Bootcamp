ac573a2267b421ab9e5fece35c7b7bd9cf543b30
showemeowkx <sashacherepov55@gmail.com>
2025-04-01 18:13:25 +0200

    Practice 1: implement Task 1.1

---
+++ generate-keypair.ts
@@ -0,0 +1,7 @@
+import { Keypair } from "@solana/web3.js";
+
+const keypair = Keypair.generate();
+
+console.log("Public key:", keypair.publicKey.toBase58());
+console.log("Secret key:", keypair.secretKey);
+console.log("Generated successfully!");
