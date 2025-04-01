2b43bd4e7f4212c2b4f59bdc0350461b5a8496c0
showemeowkx <sashacherepov55@gmail.com>
2025-04-01 19:06:49 +0200

    feat (Practice 1: Task 1.3): Airdrop some SOL to the balance

--- check-balance.ts
+++ check-balance.ts
@@ -7,6 +7,7 @@
     PublicKey,
     clusterApiUrl,
   } = require("@solana/web3.js");
+  const { airdropIfRequired } = require("@solana-developers/helpers");
 
   const connection = new Connection(clusterApiUrl("devnet"));
   console.log("Connected to devnet!");
@@ -15,10 +16,17 @@
     "Gxu6sTPutrry9PdBixkdGmhXwzrKG1neXBcD8wUH9EE5"
   );
 
+  await airdropIfRequired(
+    connection,
+    publicKey,
+    5 * LAMPORTS_PER_SOL,
+    1 * LAMPORTS_PER_SOL
+  );
+
   const balanceInLamports = await connection.getBalance(publicKey);
   const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
 
   console.log(
-    `The balance of the wallet at address ${publicKey} is ${balanceInSOL}`
+    `The balance of the wallet at address ${publicKey} is ${balanceInSOL} SOL`
   );
 })();
